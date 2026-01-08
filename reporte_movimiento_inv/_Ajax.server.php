09607e0751c5df7f0ed0da191783b3105ad3fc82
ERIK QUISNIA <105946950+erikstiven@users.noreply.github.com>
2026-01-08 13:51:14 -0500

    Refine transaction select styling

--- reporte_movimiento_inv/_Ajax.server.php
+++ reporte_movimiento_inv/_Ajax.server.php
@@ -364,7 +364,7 @@ function genera_cabecera_formulario($sAccion = 'nuevo', $aForm = '')
 
             $ifu->AgregarCampoFecha('fecha_fin', 'Fecha Final|left', true, date('Y') . '/' . date('m') . '/' . date('d'), 70,20,true);
 
-            $select .= '<select multiple name="tran" id="tran" class="form-control input-sm"></select>';
+            $select .= '<select multiple name="tran" id="tran" class="form-control input-sm tran-select"></select>';
 
             break;
 
@@ -384,7 +384,7 @@ function genera_cabecera_formulario($sAccion = 'nuevo', $aForm = '')
 
             $ifu->cCampos["empresa"]->xValor = $empresa;
 
-            $select .= '<select multiple name="tran" id="tran" class="form-control input-sm"></select>';
+            $select .= '<select multiple name="tran" id="tran" class="form-control input-sm tran-select"></select>';
 
             break;
 
@@ -402,7 +402,7 @@ function genera_cabecera_formulario($sAccion = 'nuevo', $aForm = '')
 																s.subo_cod_empr = $empresa and
 																s.subo_cod_sucu = $sucursal", true, 170, 150,true);
 
-            $select .= '<select multiple name="tran" id="tran" size="5" class="form-control input-sm">';
+            $select .= '<select multiple name="tran" id="tran" size="5" class="form-control input-sm tran-select">';
             $query = "select tran_cod_tran, tran_des_tran from saetran where tran_cod_empr = $empresa and tran_cod_sucu = $sucursal and tran_cod_modu = 10 order by tran_des_tran";
             if ($oIfx->Query($query)) {
                 if ($oIfx->NumFilas() > 0) {
